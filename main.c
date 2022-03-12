da487e347598e4983cd54b7359ca909bdf36f1a2
musyokamwania <99266001+musyokamwania@users.noreply.github.com>
2022-03-12 11:52:58 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,34 +1,43 @@
-/*
-COUNTY LIBRARY MANAGEMENT SYSTEM
-By patrick mwania
-Feb 2022
-MIT license
-C89 Compiler
-*/
 #include <stdio.h>
 #include <stdlib.h>
 
-int Menu()
+int main()
 {
-    int action;
-    printf("select an action below\n");
-    printf("1. add new patron /n");
-    printf("2. view all patrons/n");
-    printf("3. view all books/ n");
-    printf("4. add new book /n");
-    printf(" your action:");
-    scanf("%d",&action);
+    printf("2. view patrons!\n");
+    printf("3.view books!\n");
+    printf("4.add new book!\n");
+    printf("your action!\n");
+    scanf("%d,&action");
+    if(action< 1||action >4) {
+            ptintf("invalid action.try again!!");
 
+    }
     return action;
+ void execute action(int action) {
+     switch(action) {
+     case 1:
+        printf("adding a new patron\n");
+        break;
+    case2 :
+        printf("list of all patrons\n");
+        break;
+    case3 :
+        printf("list of all books\n");
+        break;
+   case4 :
+       printf("adding a new book\n");
+       break;
+    default:printf("invalid action.\n");
 
-}
-int main(){
+     }
 
-    printf("COUNTY LIBRARY STSTEM\n");
-    printf("welcome  Mr.patrick\n");
-    printf("you selected action %d",Menu());
+ int main()
+ {
+    printf("COUNTY  LIBRARY SYSTEM!\n");
+    printf("WELCOMEPATRICK!\n");
+    execute action(menu());
+
+ }
 
     return 0;
 }
-
-
